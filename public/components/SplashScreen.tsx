85e2940c863d0ce815729918514537ba2a94c4b3
012anjaliRodrigues <138094297+012anjaliRodrigues@users.noreply.github.com>
2026-04-01 12:09:49 +0530

    video in public folder

--- public/components/SplashScreen.tsx
+++ public/components/SplashScreen.tsx
@@ -69,7 +69,7 @@ const SplashScreen: React.FC<SplashScreenProps> = ({ onFinished }) => {
           style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block' }}
         >
           {/* Place splash.mp4 in your /public folder */}
-          <source src="/splashh.mp4" type="video/mp4" />
+          <source src="assets/splashh.mp4" type="video/mp4" />
         </video>
       </div>
 
